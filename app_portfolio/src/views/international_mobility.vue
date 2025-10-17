e8f3840d08076125b9c6df48f21c3598d56fad44
t70n <97734858+t70n@users.noreply.github.com>
2025-10-17 13:58:15 +0200

    Mobility Update

--- app_portfolio/src/views/international_mobility.vue
+++ app_portfolio/src/views/international_mobility.vue
@@ -4,17 +4,51 @@
         <h1>International Mobility Projects</h1>
 
         <div>
-            <h2>International Mobility Projects</h2>
-            <p>Through my interviews and meetings with my mentors (which you can find <router-link to="/career_development">here</router-link> ), I developed three personal projects aligned with their fields of expertise.</p>
-            <div> 
-                <h3><a href="https://alsatis-reseaux.com/" target="_blank" rel="noopener noreferrer">Internet of Things</a></h3>
-                <p>Collaborate with Alsatis on a mission in Europe to develop new projects and contribute to the company's growth. Countries : Germany, Spain, Great Britain.</p>
-            </div>    
-            <div> 
-                <h3><a href="https://www.nasa.gov/learning-resources/internship-programs/" target="_blank" rel="noopener noreferrer">Space</a></h3>
-                <p>Contribute to international space agencies' efforts in developing technologies for future space missions. Countries : USA, China, Japan, India</p>
+            <h2>Semester Exchange</h2>
+            
+            <p>ENSEEIHT permit to student to go on an student exchange program for full semeter S9. As we are now in the applyting periode, y would like to go (by order of preference) : </p>
+            
+            <div class="container">    
+                <section class="left-section">
+                    <h3><a href="https://www.ntnu.edu/" target="_blank">Norway : Norwegian University of Science and Technology</a></h3>
+                    <p>I would like to follow the following courses : </p>
+                    <ul>
+                        <li><a href="https://www.ntnu.edu/studies/courses/TTT4234/2025/1#tab=omEmnet" target="_blank">Space Technology I</a></li>
+                        <li><a href="https://www.ntnu.edu/studies/courses/TTM4536/2025/1#tab=omEmnet" target="_blank">Advanced Ethical Hacking</a></li>
+                        <li><a href="https://www.ntnu.edu/studies/courses/IIK3100/2025/1#tab=omEmnet" target="_blank">Ethical Hacking and Penetration Testing</a></li>
+                        <li><a href="https://www.ntnu.edu/studies/courses/TTM4138/2025/1#tab=omEmnet" target="_blank">Wireless Network Security</a></li>
+                        <li><a href="https://www.ntnu.edu/studies/courses/IIK8105/2025/1#tab=omEmnet" target="_blank">Post-Quantum Cryptography</a></li>
+                        <li><a href="https://www.ntnu.edu/studies/courses/IDATT2503/2025/1#tab=omEmnet" target="_blank">Security in programming and cryptography</a></li>
+                        <li><a href="https://www.ntnu.edu/studies/courses/IT3708/2025/1#tab=omEmnet" target="_blank">Bio-Inspired Artificial Intelligence</a></li>
+                    </ul>
+                </section>
+                <section class="right-section">
+                    <iframe
+                        width="100%"
+                        height="100%"
+                        src="https://www.youtube.com/embed/cIgKd1SwGLI"
+                        title="YouTube video player"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen>
+                    </iframe>
+                </section>
             </div>
-            <div> 
+            <div class="container">    
+                <section class="left-section">
+                    <h3><a href="https://www.ltu.se/" target="_blank">Sweden : Luleå University of Technology</a></h3>
+                    <p>I would like to follow the following courses : </p>
+                    <ul>
+                        <li><a href="https://www.ltu.se/en/education/syllabuses/course-syllabus?id=D7042E" target="_blank">IoT-based industrial automation and digitalization</a></li>
+                        <li><a href="https://www.ltu.se/en/education/syllabuses/course-syllabus?id=D7018B" target="_blank">Industrial AI for Operation and Maintenance</a></li>
+                        <li><a href="https://www.ltu.se/en/education/syllabuses/course-syllabus?id=D7070E" target="_blank">Foundations of Private Key Cryptography</a></li>
+                    </ul>
+                </section>
+                <section class="right-section">
+                    <img class="custom-image" src="/img/lulea.jpg" alt="HomeBox"/>
+                </section>  
+            </div>
+            <div>
                 <h3><a href="https://www.ericsson.com/en" target="_blank" rel="noopener noreferrer">Cybersecurity</a></h3>
                 <p>Contribute to the IoT ecosystem in Sweden by developing innovative cybersecurity solutions for smart cities. Sweden cities : Stockholm, Gothenburg, Lund</p>
             </div>  
