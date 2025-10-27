6b008838f371ef2ca96d3d241317dccc5abc7de5
t70n <97734858+t70n@users.noreply.github.com>
2025-10-27 13:12:50 +0100

    Github Pages Configuration

--- src/router.js
+++ src/router.js
@@ -16,7 +16,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL), // Utilise BASE_URL défini dans vite.config.js
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes
 });
 
