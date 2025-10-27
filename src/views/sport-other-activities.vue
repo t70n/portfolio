129afbd1361b4da9a0ef6ec53ab45b770fb185cd
t70n <97734858+t70n@users.noreply.github.com>
2025-10-27 22:29:01 +0100

    fix 404 error

--- src/views/sport-other-activities.vue
+++ src/views/sport-other-activities.vue
@@ -5,7 +5,8 @@
         <div class="container">
             <section class="split-section">
                 <h2 class="centered">Climbing</h2>
-                <p>After many years of practicing martial arts (see my earlier post), I’ve discovered another sporting passion: rock climbing. I mainly do bouldering, drawn to the sport for both its physical demands and the mental challenge it presents.</p> 
+                <p>After many years of practicing martial arts (see my earlier post), I’ve discovered another sporting passion: rock climbing.</p>
+                <p>I mainly do bouldering, drawn to the sport for both its physical demands and the mental challenge it presents.</p> 
                 <p>Every climb requires strategy and problem-solving, making it as much a test of the mind as it is of strength and agility.</p>
                 <p>Over the next few months, I plan to upgrade my climbing by tackling lead climbing and outdoor routes.</p>
                 <div class="container">
@@ -18,24 +19,8 @@
                 </div>
             </section>
             <section class="split-section">
-                <h2 class="centered">Martial Arts</h2>
-                <p>Raised since childhood with the values of martial arts, I practiced <a href="https://www.karate-bergerac.com/" target="_blank" rel="noopener">Karate Gojo-Ryu</a> style for many years under JUNDOKAN International. I earned my black belt in Portugal. This style, focused on the excellence of techniques applied in katas, taught me resilience in work, determination, and instilled in me the power of learning through repetition.</p>
-                <p>Later, craving more free-fighting and contact, I turned to combat jujitsu, where I discovered a particular appetite for ground fighting.</p>
-                <div class="container">
-                    <section class="split-section">
-                        <img src="/img/moto1.jpeg" alt="Ride in the Cantal"/>
-                    </section>
-                    <section class="split-section">
-                        <img src="/img/moto2.jpeg" alt="Flower Helmet"/>
-                    </section>
-                </div>
-            </section>
-        </div>
-        <div class="container">
-            <section class="split-section">
                 <h2 class="centered">Music</h2>
-                <p>As a child, I started playing guitar at my town’s music school, where I developed a passion for the instrument. I played in several bands through the Rockschool initiative. Later, I took up drums to mix things up and explore new joys.</p>
-                <p>More recently, when I started at ENSEEIHT, I joined a band called NightSuze, a rock cover band playing artists like Muse, Måneskin, Bruno Mars, and more (see the photo from our first gig at the student union).</p>
+                <p>As a child, I started playing guitar at my town’s music school, where I developed a passion for the instrument. I played in several bands through the Rockschool initiative. Later, I took up drums to mix things up and explore new joys. More recently, when I started at ENSEEIHT, I joined a band called NightSuze, a rock cover band playing artists like Muse, Måneskin, Bruno Mars, and more (see the photo from our first gig at the student union).</p>
                 <p>Currently, I’m leaning toward a more classical style, finding joy in playing video game soundtracks like <a href="https://en.wikipedia.org/wiki/The_Last_of_Us" target="_blank" rel="noopener">The Last of Us</a> or <a href="https://en.wikipedia.org/wiki/Clair_Obscur:_Expedition_33" target="_blank" rel="noopener">Clair Obscur Expedition 33</a>.</p>
                 <div class="container">
                     <section class="split-section">
@@ -43,6 +28,22 @@
                     </section>
                     <section class="split-section">
                         <img src="/img/concert_foy2.jpg" title="Night Suze Concert n°2 at FOY N7 - Credit Photo : Photo7" alt="Night Suze Concert n°2 at FOY N7 - Credit Photo : Photo7"/>
+                    </section>
+                </div>
+            </section>
+        </div>
+        <div class="container">
+            <section class="split-section">
+                <h2 class="centered">Martial Arts</h2>
+                <p>Raised since childhood with the values of martial arts, I practiced <a href="https://www.karate-bergerac.com/" target="_blank" rel="noopener">Karate Gojo-Ryu</a> style for many years under JUNDOKAN International. I earned my black belt in Portugal.</p>
+                <p>This style, focused on the excellence of techniques applied in katas, taught me resilience in work, determination, and instilled in me the power of learning through repetition.</p>
+                <p>Later, craving more free-fighting and contact, I turned to combat jujitsu, where I discovered a particular appetite for ground fighting.</p>
+                <div class="container">
+                    <section class="split-section">
+                        <img src="/img/karate.jpeg" alt="Ride in the Cantal"/>
+                    </section>
+                    <section class="split-section">
+                        <img src="/img/jujitsu.jpg" alt="Flower Helmet"/>
                     </section>
                 </div>
             </section>
